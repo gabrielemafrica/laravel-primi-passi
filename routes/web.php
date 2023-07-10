e79df7cf5882f1e5fe6c7f40ed4945c7305a59f3
gabrielemafrica <gabriele.ga@live.it>
2023-07-10 19:07:32 +0300

    primo saluto

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// home
+Route::get('/home', function () {
+    $saluto = 'Hello';
+    $oggetto = 'Laravel';
+    return view('home', compact('saluto', 'oggetto'));
+});
